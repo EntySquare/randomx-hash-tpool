8d1c5adceeb28b88b0fbabc307b3a0811a50aba9
Tsang <314156936@qq.com>
2022-04-20 20:04:16 +0800

    init submodules

--- hash_pool.c
+++ hash_pool.c
@@ -10,7 +10,7 @@ int main() {
     const char myInput[] = "RandomX example input";
     char hash[RANDOMX_HASH_SIZE];
 
-//    randomx_flags flags = randomx_get_flags();
+    randomx_flags flags = randomx_get_flags();
 //    randomx_cache *myCache = randomx_alloc_cache(flags);
 //    randomx_init_cache(myCache, &myKey, sizeof myKey);
 //    randomx_vm *myMachine = randomx_create_vm(flags, myCache, NULL);
