9587ef85d1d49e4a6f6472d129a9d1059e9baade
zhurongji <506765498@qq.com>
2022-04-20 18:02:00 +0800

    hash_pool

--- hash_pool.c
+++ hash_pool.c
@@ -4,25 +4,32 @@
 
 #include "lib/RandomX/src/randomx.h"
 #include <stdio.h>
+#include <list>
 
-int main() {
-    const char myKey[] = "RandomX example key";
-    const char myInput[] = "RandomX example input";
-    char hash[RANDOMX_HASH_SIZE];
+static ERL_NIF_TERM randomx_hash_thread_nif( const void key, const void hashData, const void num)
 
-    randomx_cache *myCache = randomx_alloc_cache(RANDOMX_FLAG_DEFAULT);
-    randomx_init_cache(myCache, &myKey, sizeof myKey);
-    randomx_vm *myMachine = randomx_create_vm(RANDOMX_FLAG_DEFAULT, myCache, NULL);
+{
+    int main() {
+        const char myKey[] = key; //"RandomX example key";
+        const char myInput[] = hashData; //"RandomX example input";
+        const char RANDOMX_HASH_SIZE = 10;
+        char hash[RANDOMX_HASH_SIZE];
+        list<int> l1;
 
-    randomx_calculate_hash(myMachine, &myInput, sizeof myInput, hash);
+        randomx_cache *myCache = randomx_alloc_cache(RANDOMX_FLAG_DEFAULT);
+        randomx_init_cache(myCache, &myKey, sizeof myKey);
+        randomx_vm *myMachine = randomx_create_vm(RANDOMX_FLAG_DEFAULT, myCache, NULL);
 
-    randomx_destroy_vm(myMachine);
-    randomx_release_cache(myCache);
+        randomx_calculate_hash(myMachine, &myInput, sizeof myInput, hash);
 
-    for (unsigned i = 0; i < RANDOMX_HASH_SIZE; ++i)
-        printf("%02x", hash[i] & 0xff);
+        randomx_destroy_vm(myMachine);
+        randomx_release_cache(myCache);
 
-    printf("\n");
-
-    return 0;
+        for (unsigned i = 0; i < RANDOMX_HASH_SIZE; ++i){
+            printf("%02x", hash[i] & 0xff);
+        printf("\n");
+        l1.insert(i, hash[i]);
+        }
+        return l1;
+        }
 }
