104b49d0a386455141d3b5094e9711a15a71e6fb
zhurongji <506765498@qq.com>
2022-04-21 18:42:02 +0800

    hash_pool

--- hash_pool_simplr.c
+++ hash_pool_simplr.c
@@ -43,11 +43,11 @@ int main()
 
     printf("input ready ==\n");
 
-            const int length_h0 = sizeof(h0)/sizeof(unsigned char);
-            const int length_prevh = sizeof(prevh)/sizeof(unsigned char);
-            const int length_time = sizeof(timestampBinary)/sizeof(unsigned char);
-            const int length_chunk = sizeof(chunk)/sizeof(unsigned char);
-            const int length_entropy = sizeof(entropy)/sizeof(unsigned char);
+            const int length_h0 = sizeof(h0);
+            const int length_prevh = sizeof(prevh);
+            const int length_time = sizeof(timestampBinary);
+            const int length_chunk = sizeof(chunk);
+            const int length_entropy = sizeof(entropy);
 
             printf("input ready ==, length is %n\n", length_chunk);
 
