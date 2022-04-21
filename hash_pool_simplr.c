307bf97b510c7f63dad6535004bbef569c452974
zhurongji <506765498@qq.com>
2022-04-21 21:36:46 +0800

    hash_pool

--- hash_pool_simplr.c
+++ hash_pool_simplr.c
@@ -31,14 +31,14 @@
 
 int main()
 {
-            const unsigned char myKey[] = {255, 255,255, 254, 219, 155, 62, 29, 172, 210, 122, 149, 253, 169, 34, 24,
+            const int myKey[] = {255, 255,255, 254, 219, 155, 62, 29, 172, 210, 122, 149, 253, 169, 34, 24,
                                            33, 152, 221, 38, 200, 234, 74, 60, 118, 235, 15, 159, 33, 237, 210, 127};
-            const unsigned char h0[] = {236,97,53,71,37,0,200,215,7,52,32,198,108,183,90,4,140,41,110,170,32,109,7,56,229,47,186,12,150,63,52,232};
-            const unsigned char prevh[] = {61, 222, 227, 151, 197, 175, 127, 142, 18, 210, 148, 122, 239, 9, 40, 9, 78, 47, 1, 208, 199, 19, 214, 225, 211, 93, 196, 144, 253, 232, 176, 145, 62, 172, 183, 229, 89, 16, 42, 96, 247, 44, 228, 20, 71, 71, 31, 85};
-            const unsigned char timestampBinary[] = {0,0,0,0,0,0,0,0,98,93,21,92};
-            const unsigned char chunk[] = RANDOMX_HASH_TPOOL_CHUNK_AND_ENTROPY;
-            const unsigned char entropy[] = RANDOMX_HASH_TPOOL_CHUNK_AND_ENTROPY;
-            unsigned char myInput[] = {0};
+            const int h0[] = {236,97,53,71,37,0,200,215,7,52,32,198,108,183,90,4,140,41,110,170,32,109,7,56,229,47,186,12,150,63,52,232};
+            const int prevh[] = {61, 222, 227, 151, 197, 175, 127, 142, 18, 210, 148, 122, 239, 9, 40, 9, 78, 47, 1, 208, 199, 19, 214, 225, 211, 93, 196, 144, 253, 232, 176, 145, 62, 172, 183, 229, 89, 16, 42, 96, 247, 44, 228, 20, 71, 71, 31, 85};
+            const int timestampBinary[] = {0,0,0,0,0,0,0,0,98,93,21,92};
+            const int chunk[] = RANDOMX_HASH_TPOOL_CHUNK_AND_ENTROPY;
+            const int entropy[] = RANDOMX_HASH_TPOOL_CHUNK_AND_ENTROPY;
+             int myInput[] = {0};
             char hash[RANDOMX_HASH_SIZE];
 
             int len_h0 = sizeof(h0)/sizeof(int);
