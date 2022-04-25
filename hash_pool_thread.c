d30f2b1419033b6e8ce87df367e8d1e18c76b907
zhurongji <506765498@qq.com>
2022-04-25 17:06:37 +0800

    hash_pool

--- hash_pool_thread.c
+++ hash_pool_thread.c
@@ -54,9 +54,11 @@ void *hash_cal(void *paramsPtr)
 
     randomx_cache *myCache = randomx_alloc_cache(flags_fast);
     randomx_init_cache(myCache, ((struct param*)paramsPtr)->key, ((struct param*)paramsPtr)->keySize);
+    randomx_cache *myDataset = randomx_alloc_dataset(flags_fast);
+    init_dataset(myDataset, myCache, 64);
     randomx_release_cache(myCache);
     myCache = NULL;
-    randomx_vm *myMachine = randomx_create_vm(flags_vm, myCache, randomx_alloc_dataset(flags_fast));
+    randomx_vm *myMachine = randomx_create_vm(flags_vm, myCache, myDataset);
 //    printf("the flag is %d\n", flags);
 
     time_t start = time(NULL);
