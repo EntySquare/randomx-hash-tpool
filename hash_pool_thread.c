7e9786d926f8edbb39abdd6eeb1b6923f51991bb
zhurongji <506765498@qq.com>
2022-04-25 16:51:04 +0800

    hash_pool

--- hash_pool_thread.c
+++ hash_pool_thread.c
@@ -40,10 +40,10 @@ void *hash_cal(void *paramsPtr)
     //long tid = ((struct param*)paramsPtr)->threadnum;
     printf("Thread starting...\n");
 
-    randomx_flags flags = randomx_get_flags();
-//    randomx_flags flags_vm = RANDOMX_FLAG_FULL_MEM;
-    flags |= RANDOMX_FLAG_HARD_AES;
-    flags |= RANDOMX_FLAG_JIT;
+//    randomx_flags flags = randomx_get_flags();
+    randomx_flags flags_vm = RANDOMX_FLAG_FULL_MEM;
+    flags_vm |= RANDOMX_FLAG_HARD_AES;
+    flags_vm |= RANDOMX_FLAG_JIT;
 
     randomx_flags flags_fast = RANDOMX_FLAG_DEFAULT;
     flags_fast |= RANDOMX_FLAG_JIT;
@@ -52,9 +52,10 @@ void *hash_cal(void *paramsPtr)
 //        flags |= RANDOMX_FLAG_LARGE_PAGES;
 //    }
 
-    randomx_cache *myCache = randomx_alloc_cache(flags);
+    randomx_cache *myCache = randomx_alloc_cache(flags_fast);
     randomx_init_cache(myCache, ((struct param*)paramsPtr)->key, ((struct param*)paramsPtr)->keySize);
-    randomx_vm *myMachine = randomx_create_vm(flags, myCache, randomx_alloc_dataset(flags));
+    randomx_release_cache(myCache);
+    randomx_vm *myMachine = randomx_create_vm(flags_vm, myCache, randomx_alloc_dataset(flags_fast));
 //    printf("the flag is %d\n", flags);
 
     time_t start = time(NULL);
@@ -77,7 +78,6 @@ void *hash_cal(void *paramsPtr)
     }
 
     randomx_destroy_vm(myMachine);
-    randomx_release_cache(myCache);
 
     pthread_exit( (void*) paramsPtr);
 
