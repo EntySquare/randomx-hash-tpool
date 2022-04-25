4f103c388d041e4519c800626848d4a5de171178
zhurongji <506765498@qq.com>
2022-04-25 19:21:14 +0800

    hash_pool

--- hash_pool_thread.c
+++ hash_pool_thread.c
@@ -43,8 +43,9 @@ void *hash_cal(void *paramsPtr)
 //   randomx_flags flags_vm = RANDOMX_FLAG_DEFAULT;
 //    flags_vm |= RANDOMX_FLAG_HARD_AES;
 //    flags_vm |= RANDOMX_FLAG_JIT;
-    randomx_flags flags_fast = RANDOMX_FLAG_DEFAULT;
-    flags_fast |= RANDOMX_FLAG_JIT;
+//    randomx_flags flags_fast = RANDOMX_FLAG_DEFAULT;
+//    flags_fast |= RANDOMX_FLAG_JIT;
+    randomx_flags flags_vm = 8;
     randomx_flags flags_vm = 15;
     printf("flags is %d\n", flags_vm);
 
