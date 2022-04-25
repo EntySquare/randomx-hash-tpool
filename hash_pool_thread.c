a5944aa5efcea8d79c43ce99cfc5fc84fa981b85
zhurongji <506765498@qq.com>
2022-04-25 12:45:18 +0800

    hash_pool

--- hash_pool_thread.c
+++ hash_pool_thread.c
@@ -125,10 +125,10 @@ int main()
     for (int j = 0; j<thread_count ; j++)
     {
         pthread_create(&thread_id[j], NULL, hash_cal, (void *) parameters);
-//        pthread_join(thread_id[j], NULL);
+        pthread_join(thread_id[j], NULL);
     }
-    for (int k = 0; k<thread_count ; k++)
-    {pthread_join(thread_id[k], NULL);}
+//    for (int k = 0; k<thread_count ; k++)
+//    {pthread_join(thread_id[k], NULL);}
 
 //    randomx_calculate_hash(myMachine, &myInput, sizeof myInput, hash);
 //    for (unsigned i = 0; i < RANDOMX_HASH_SIZE; ++i)
