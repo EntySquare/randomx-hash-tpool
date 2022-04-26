746d37b17946a3af95e35624c63fce53c4ec1ba5
Tsang <314156936@qq.com>
2022-04-26 16:44:59 +0800

    init submodules

--- cpu.h
+++ cpu.h
@@ -1,27 +1,27 @@
+////
+//// Created by luo2 on 2022/4/26.
+////
 //
-// Created by luo2 on 2022/4/26.
 //
-
-
-#ifndef _CPU_H
-#define _CPU_H
-
-
-#ifdef __cplusplus
-extern "C"
-{
-#endif
-
-#define _GNU_SOURCE
-#include <sched.h>
-#include <pthread.h>
-
-void print_cpu_affinity_id(int core_nums, pthread_t tid, cpu_set_t* cpu_set);
-int frank_phtread_cpu_affinity_get(pthread_t tid, cpu_set_t* cpu_set);
-int frank_pthread_single_cpu_affinity_set(int core_id, pthread_t tid);
-
-
-#ifdef __cplusplus
-}
-#endif
-#endif //_CPU_H
+//#ifndef _CPU_H
+//#define _CPU_H
+//
+//
+//#ifdef __cplusplus
+//extern "C"
+//{
+//#endif
+//
+//#define _GNU_SOURCE
+//#include <sched.h>
+//#include <pthread.h>
+//
+//void print_cpu_affinity_id(int core_nums, pthread_t tid, cpu_set_t* cpu_set);
+//int frank_phtread_cpu_affinity_get(pthread_t tid, cpu_set_t* cpu_set);
+//int frank_pthread_single_cpu_affinity_set(int core_id, pthread_t tid);
+//
+//
+//#ifdef __cplusplus
+//}
+//#endif
+//#endif //_CPU_H
