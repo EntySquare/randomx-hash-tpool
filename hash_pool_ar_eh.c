81d44ea31f3f16a552e23568d8fda4f6e6068499
zhurongji <506765498@qq.com>
2022-04-26 17:05:03 +0800

    hash_pool

--- hash_pool_ar_eh.c
+++ hash_pool_ar_eh.c
@@ -80,10 +80,10 @@ int frank_pthread_single_cpu_affinity_set(int core_id, pthread_t tid)
 
     CPU_ZERO(&mask);
     CPU_SET(core_id, &mask);
-    printf("cpu\n");
+    printf("mask is %s, and tid is %s\n", mask, tid);
     if (pthread_setaffinity_np(tid, sizeof(cpu_set_t), &mask) < 0)
     {
-        //fprintf(stderr, "set thread[%x] affinity failed\n", (unsigned int)tid);
+        fprintf(stderr, "set thread[%x] affinity failed\n", (unsigned int)tid);
         return 1;
     }
     printf("done\n");
