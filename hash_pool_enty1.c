4ce2ea0253723eaaca1bb88e514790e1a8d33c0f
zhurongji <506765498@qq.com>
2022-04-27 21:54:52 +0800

    hash_pool

--- hash_pool_enty1.c
+++ hash_pool_enty1.c
@@ -41,7 +41,8 @@ struct param {
     unsigned char* output;
 };
 
-struct param *parameters[THREADS_COUNT] = (struct param *) calloc(THREADS_COUNT, sizeof(struct param));
+struct param *parameters[1] = (struct param *) calloc(THREADS_COUNT, sizeof(struct param));
+struct param *parameters[2] = (struct param *) calloc(THREADS_COUNT, sizeof(struct param));
 
 //void hash_cal(randomx_vm *machine, const void *input, size_t inputSize, void *output)
 void *hash_cal(void *paramsPtr)
