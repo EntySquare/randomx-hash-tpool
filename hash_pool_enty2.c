e5e43071b50a8d9e6d94ce456f128660487cf274
zhurongji <506765498@qq.com>
2022-05-09 21:10:28 +0800

    hash_pool

--- hash_pool_enty2.c
+++ hash_pool_enty2.c
@@ -180,10 +180,9 @@ int main()
     printf("chunk file length is %d\n", nLen);
     int nRead = CHUNK_ENTROPY_SIZE ;
     fread(chunk_data, 1, nRead , chunk_file);
-    for(int j=0; j<CHUNK_ENTROPY_SIZE; j++){
+    for(int j=0; j<20; j++){
     printf( "%d,", chunk_data[j]);}
     printf( "\n");
-    //memset( chunk_data, 0x0, sizeof(chunk_data) );
     fclose(chunk_file);
 
 
