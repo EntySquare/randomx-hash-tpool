e9cf9928a3c9e0669f4e888980914eae9a8393e3
zhurongji <506765498@qq.com>
2022-05-09 21:05:49 +0800

    hash_pool

--- hash_pool_enty2.c
+++ hash_pool_enty2.c
@@ -174,8 +174,8 @@ int main()
     FILE *chunk_file = NULL;
     unsigned char chunk_data[CHUNK_ENTROPY_SIZE] = {0};
     chunk_file = fopen( "/ardir/ar_chunk_storage1/10028580864000", "r+");
-    int offset = 2;
-    fseek(chunk_file, offset, SEEK_SET);  //locate at offset
+    int offset = 10;
+    fseek(chunk_file, offset+3, SEEK_SET);  //locate at offset
     int nLen = ftell(chunk_file);   //get the whole length of the file
     printf("chunk file length is %d\n", nLen);
     int nRead = CHUNK_ENTROPY_SIZE ;
